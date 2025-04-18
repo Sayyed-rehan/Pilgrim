4cf552242ce8c3e211cff29ee74112b9f21eaeb1
Sayyed-rehan <rehansayyed1216@gmail.com>
2025-04-18 20:45:03 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Feed from './Pages/Feed/Feed'
 import ProductConfig from './Pages/Feed/ProductConfig/ProductConfig'
-import {BrowserRouter, Routes, Route} from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 
 const App = () => {
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Feed/>}/>
-    <Route path="/product" element={<ProductConfig/>}/>
-    </Routes>
-    </BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/product" element={<ProductConfig />} />
+      </Routes>
     </div>
   )
 }
